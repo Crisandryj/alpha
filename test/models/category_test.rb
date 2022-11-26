6c18a3c7839f0f32cbdf3a9fd3cfcfb4edd7b219
crisandryj <crisandryj@gmail.com>
2022-11-26 13:35:31 -0500

    created models test

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,14 +1,34 @@
  require 'test_helper'
 
  class CatergoryTest < ActiveSupport::TestCase
-   test "catergory should be vaild" do
+
+   def setup
      @category = Category.new(name: "Sports")
-     assert @category.valid?
    end
 
    test "catergory should be vaild" do
-     @category = Category.new(name: "Sports")
      assert @category.valid?
+   end
+
+   test "should be present" do
+     @category.name = " "
+     assert_not @category.valid?
+   end
+
+   test "name should be unique" do
+      @category.save
+      @category2 = Category.new(name: "Sports")
+      assert_not @category2.valid?
+   end
+
+   test "name should not be too long" do
+     @category.name = "a" * 26
+     assert_not @category.valid?
+   end
+
+   test "name should not be too short" do
+     @category.name = "a" * 2
+     assert_not @category.valid?
    end
 
  end
